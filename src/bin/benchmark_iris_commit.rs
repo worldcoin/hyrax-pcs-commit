8c4aa7c267931b07d24bd31577b778da3fd8a831
vishady <vishruti721@gmail.com>
2024-03-19 17:34:36 -0700

    more comments

--- src/bin/benchmark_iris_commit.rs
+++ src/bin/benchmark_iris_commit.rs
@@ -15,17 +15,17 @@ type Scalar = <Bn256 as PrimeOrderCurve>::Scalar;
 
 const N_ROWS: usize = 1 << 8;
 const N_COLS: usize = 1 << 9;
+const FILENAME: &str = "test.json";
 
-fn compute_commitment(
-    filepath: &str,
-    n_rows: usize,
-    n_cols: usize,
-    log_split_point: usize,
-) -> Vec<Bn256> {
+fn compute_commitment(n_rows: usize, n_cols: usize, log_split_point: usize) -> Vec<Bn256> {
     // --- Generating random matrix to be committed to; same size as iris image ---
     let u8_matrix = (0..n_rows * n_cols)
         .map(|_| rand::random::<u8>())
         .collect_vec();
+    // generate a Pedersen Committer struct that includes all of the generators (`g_i`) needed in order
+    // to commit to the message, along with the generator `h` that is needed in order to blind the message.
+    // uses the default constructor which samples the generators deterministically using the Shake256 hash function.
+    // public string is arbitrary for now.
     let vector_committer: PedersenCommitter<Bn256> = PedersenCommitter::new(
         N_COLS,
         "accountable magic abcdefghijklmnopqrstuvwxyz",
@@ -41,7 +41,7 @@ fn compute_commitment(
         compute_matrix_commitments(log_split_point, &u8_matrix, &vector_committer, seed);
 
     // --- Serialization ---
-    let file = fs::File::create(filepath).unwrap();
+    let file = fs::File::create(FILENAME).unwrap();
     let bw = BufWriter::new(file);
     serde_json::to_writer(bw, &commitment).unwrap();
 
@@ -53,7 +53,7 @@ fn compute_commitment(
 fn main() {
     let start_time = Instant::now();
     let log_split_point = 9;
-    let commit = compute_commitment("test6.json", N_ROWS, N_COLS, log_split_point);
+    let commit = compute_commitment(N_ROWS, N_COLS, log_split_point);
     println!(
         "Computing {} vector commitments, each of length {}, took: {:?}",
         N_ROWS,
@@ -62,7 +62,7 @@ fn main() {
     );
 
     // testing deserialization
-    let mut file = std::fs::File::open("test6.json").unwrap();
+    let mut file = std::fs::File::open(FILENAME).unwrap();
     let initial_buffer_size = file.metadata().map(|m| m.len() as usize + 1).unwrap_or(0);
     let mut bufreader = Vec::with_capacity(initial_buffer_size);
     file.read_to_end(&mut bufreader).unwrap();
